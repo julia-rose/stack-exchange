ad25aa78c104a2e129a65aa75d8ff67f236bd662
Julia Rose <jmrose@pbs.org>
2017-07-06 17:49:53 -0400

    add option to search by tag

--- src/components/page-container/index.js
+++ src/components/page-container/index.js
@@ -7,29 +7,37 @@ class PageContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      searchBy: 'phrase'
     };
     this.setNewSearchTerm = this.setNewSearchTerm.bind(this);
+    this.setSearchBy = this.setSearchBy.bind(this);
   }
 
   setNewSearchTerm(newSearchTerm) {
     this.setState({ searchTerm: newSearchTerm });
   }
 
+  setSearchBy(newSearchBy) {
+    this.setState({ searchBy: newSearchBy });
+  }
+
   render() {
     return (
       <div className="page-container">
-        <h2 className="page-container__title">
+        <h2 className="page-container__intro">
           Confused? Overwhelmed? Utterly bewildered?
         </h2>
-        <h1 className="page-container__subtitle">
+        <h1 className="page-container__title">
           Search Stack Overflow!
         </h1>
         <SearchContainer
-          setNewSearchTerm={this.setNewSearchTerm} />
+          setNewSearchTerm={this.setNewSearchTerm}
+          setSearchBy={this.setSearchBy} />
         { this.state.searchTerm.length > 0 ?
           <ResultsContainer
-            searchTerm={this.state.searchTerm} />
+            searchTerm={this.state.searchTerm}
+            searchBy={this.state.searchBy} />
         : null }
       </div>
     );
--- src/components/search-container/index.js
+++ src/components/search-container/index.js
@@ -5,14 +5,21 @@ class SearchContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchTerm: ''
+      searchTerm: '',
+      searchBy: 'phrase'
     };
     this.handleUpdate = this.handleUpdate.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
     this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleUpdate(event) {
     this.setState({ searchTerm: event.target.value });
+  }
+
+  handleToggle(event) {
+    this.setState({ searchBy: event.target.value });
+    this.props.setSearchBy(event.target.value);
   }
 
   handleSearch() {
@@ -22,17 +29,37 @@ class SearchContainer extends Component {
   render() {
     return (
       <div className="search-container">
-        <input
-          type="text"
-          className="search-input"
-          onChange={this.handleUpdate}
-          value={this.state.searchTerm}
-        />
-        <button 
-          onClick={this.handleSearch}
-          className="search-button">
-            Search
-        </button>
+        <div className="radio-buttons">
+          <label className="option-phrase">
+            <input
+              type="radio"
+              value="phrase"
+              checked={this.state.searchBy === 'phrase'}
+              onChange={this.handleToggle} />
+            Search by phrase
+          </label>
+          <label className="option-tag">
+            <input
+              type="radio"
+              value="tag"
+              checked={this.state.searchBy === 'tag'}
+              onChange={this.handleToggle} />
+            Search by tag
+          </label>
+        </div>
+        <div className="input-container">
+          <input
+            type="text"
+            className="search-input"
+            onChange={this.handleUpdate}
+            value={this.state.searchTerm}
+          />
+          <button
+            onClick={this.handleSearch}
+            className="search-button">
+              Search
+          </button>
+        </div>
       </div>
     );
   }
